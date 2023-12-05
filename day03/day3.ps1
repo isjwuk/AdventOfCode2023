b1924a3f946510370562513d26562c3e963d5859
isjwuk <23340053+isjwuk@users.noreply.github.com>
2023-12-05 17:24:34 +0000

    framework of day 3

---
+++ day03/day3.ps1
@@ -0,0 +1,11 @@
+<#
+.SYNOPSIS
+    Solving Day 3 of the Advent of Code 2023
+#>
+
+
+$inputfilepath=".\sampledata-part1.txt"
+#$inputfilepath=".\input.txt"
+
+#Retrieve Input Data
+$inputdata=Get-Content $inputfilepath
