a6a0462e890bf7103d7e9ac6cfeb68d8484e31d6
isjwuk <23340053+isjwuk@users.noreply.github.com>
2023-12-06 19:44:21 +0000

    Works on Sample data but not my input file.

--- day03/day3.ps1
+++ day03/day3.ps1
@@ -16,6 +16,8 @@ $maxY=$inputdata.Length
 
 $digits="0123456789"
 
+$rollingTotal=0
+
 for($y=0;$y -lt $maxY; $y++){ #Loop through rows
     $line= $inputdata[$y]
     $x= $line.IndexOfAny($digits.ToString())
@@ -36,13 +38,76 @@ for($y=0;$y -lt $maxY; $y++){ #Loop through rows
         }
         
         #Show the potential part number for debugging purposes
-        "number: "+$line.Substring($x, $lastdigit-$x+1)
-      
-        
+        #"number: "+$line.Substring($x, $lastdigit-$x+1)
         #Check the surrounding cells for symbols
         #TODO
+        
+        $symbolFound=$false
+        if($y -gt 0){
+            #Check row above
+            if ($x -gt 0){
+                #Check top left corner is not a . or a number
+                if($inputdata[$y-1][$x-1] -ne "." -and !($inputdata[$y-1][$x-1] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+             #Check row directly above
+             for($i=$x;$i -le $lastdigit; $i++){
+                if($inputdata[$y-1][$i] -ne "." -and !($inputdata[$y-1][$i] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+             if ($lastdigit -lt $MaxX){
+                #Check top right corner
+                if($inputdata[$y-1][$lastdigit+1] -ne "." -and !($inputdata[$y-1][$lastdigit+1] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+        }
+        if($y -lt $maxY-1){
+            #Check row below
+            if ($x -gt 0){
+                #Check bottom left corner is not a . or a number
+                if($inputdata[$y+1][$x-1] -ne "." -and !($inputdata[$y+1][$x-1] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+             #Check row directly below
+             for($i=$x;$i -le $lastdigit; $i++){
+                if($inputdata[$y+1][$i] -ne "." -and !($inputdata[$y+1][$i] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+             if ($lastdigit -lt $MaxX){
+                #Check bottom right corner
+                if($inputdata[$y+1][$lastdigit+1] -ne "." -and !($inputdata[$y+1][$lastdigit+1] -in $digits.ToCharArray() )){
+                    $symbolFound=$true
+                }
+             }
+        }
+        #Check Left and right ends
+        if ($x -gt 0){
+            #Check left is not a . or a number
+            if($inputdata[$y][$x-1] -ne "." -and !($inputdata[$y][$x-1] -in $digits.ToCharArray() )){
+                $symbolFound=$true
+            }
+         }
+        if ($lastdigit -lt $MaxX){
+            #Check  right 
+            if($inputdata[$y][$lastdigit+1] -ne "." -and !($inputdata[$y][$lastdigit+1] -in $digits.ToCharArray() )){
+                $symbolFound=$true
+            }
+         }
+
+
+        if($symbolFound){
+            $rollingTotal+=[int]$line.Substring($x, $lastdigit-$x+1)
+            #Display for debug purposes
+            "Part Number Confirmed "+$line.Substring($x, $lastdigit-$x+1)
+        }
+
+
         #Increment X and loop round to check for more part numbers in this line
-        #$x++ #TODO- replace this line to increment X by length of potential part number just discovered
         $x=$lastdigit+1
         $firstdigit=$line.substring($x).IndexOfAny($digits.ToString())
         if ($firstdigit -ge 0) {
@@ -53,3 +118,5 @@ for($y=0;$y -lt $maxY; $y++){ #Loop through rows
     }
 
 }
+
+"Total "+[string]$rollingTotal
